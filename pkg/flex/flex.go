fa72c0cbd333f827726695434ae5fe4368537cd9
odacremolbap <pablo@stackpointcloud.com>
2017-11-21 17:08:44 +0100

    remove non needed comments

--- pkg/flex/flex.go
+++ pkg/flex/flex.go
@@ -67,33 +67,8 @@ type Command struct {
 	options  string
 }
 
-// func debugFile(msg string) {
-// 	file := "/var/log/digitalocean.log"
-// 	var f *os.File
-//
-// 	if _, err := os.Stat(file); os.IsNotExist(err) {
-// 		f, err = os.Create(file)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 	} else {
-// 		f, err = os.OpenFile(file, os.O_APPEND|os.O_WRONLY, 0600)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 	}
-// 	defer f.Close()
-//
-// 	if _, err := f.WriteString(msg + "\n"); err != nil {
-// 		panic(err)
-// 	}
-// }
-
 // NewFlexCommand given an argument list returns a Flex Command structure
 func NewFlexCommand(args []string) (*Command, error) {
-	// for _, a := range args {
-	// 	debugFile(fmt.Sprintf("%+v", a))
-	// }
 	if len(args) == 1 {
 		return nil, fmt.Errorf("no flex command argument found")
 	}
@@ -107,72 +82,37 @@ func NewFlexCommand(args []string) (*Command, error) {
 		return fc, nil
 
 	case getVolumeNameCmd:
-		// if len(fa) < 1 {
-		// 	return nil, fmt.Errorf("command %q requires options parameter.", getVolumeNameCmd)
-		// }
 		fc.options = fa[0]
 
 	case attachCmd:
-		// if len(fa) == 1 {
-		// 	// if kubelet controller attach detach has not been specified
-		// 	fc.options = fa
-		// } else if len(fa) == 2 {
 		fc.options = fa[0]
 		fc.nodeName = fa[1]
-		// } else {
-		// 	return nil, fmt.Errorf("command %q requires options/node parameter. FA: %+v", attachCmd, fa)
-		// }
 
 	case detachCmd:
-		// if len(fa) == 1 {
-		// 	// if kubelet controller attach detach has not been specified
-		// 	fc.device = fa[0]
-		// } else if len(fa) == 2 {
 		fc.device = fa[0]
 		fc.nodeName = fa[1]
-		// } else {
-		// 	return nil, fmt.Errorf("command %q requires device/node parameter. FA=%+v", detachCmd, fa)
-		// }
 
 	case waitForAttachCmd:
-		// if len(fa) != 2 {
-		// 	return nil, fmt.Errorf("command %q requires device/options parameter. FA=%+v", waitForAttachCmd, fa)
-		// }
 		fc.device = fa[0]
 		fc.options = fa[1]
 
 	case isAttachedCmd:
-		// if len(fa) != 2 {
-		// 	return nil, fmt.Errorf("command %q requires options/node parameter. FA=%+v", isAttachedCmd, fa)
-		// }
 		fc.options = fa[0]
 		fc.nodeName = fa[1]
 
 	case mountDeviceCmd:
-		// if len(fa) != 3 {
-		// 	return nil, fmt.Errorf("command %q requires mountdir/device/options parameter. FA=%+v", mountDeviceCmd, fa)
-		// }
 		fc.mountdir = fa[0]
 		fc.device = fa[1]
 		fc.options = fa[2]
 
 	case unmountDeviceCmd:
-		// if len(fa) != 1 {
-		// 	return nil, fmt.Errorf("command %q requires mountdir parameter. FA=%+v", unmountCmd, fa)
-		// }
 		fc.device = fa[0]
 
 	case mountCmd:
-		// if len(fa) != 2 {
-		// 	return nil, fmt.Errorf("command %q requires mountdir/options parameter. FA=%+v", mountCmd, fa)
-		// }
 		fc.mountdir = fa[0]
 		fc.options = fa[1]
 
 	case unmountCmd:
-		// if len(fa) != 1 {
-		// 	return nil, fmt.Errorf("command %q requires mountdir parameter. FA=%+v", unmountCmd, fa)
-		// }
 		fc.mountdir = fa[0]
 
 	default:
@@ -225,7 +165,6 @@ func (m *Manager) ExecuteCommand(fc *Command) (*DriverStatus, error) {
 	return &DriverStatus{
 		Status: StatusNotSupported,
 	}, nil
-	// return nil, fmt.Errorf("command %q not recognized as a valid flex command", fc.command)
 }
 
 // WriteError creates a Flex response containing an error
@@ -248,9 +187,7 @@ func (m *Manager) WriteDriverStatus(ds *DriverStatus) error {
 	if err != nil {
 		return fmt.Errorf("error encoding driver status to JSON: %s", err.Error())
 	}
-	// debugFile(fmt.Sprintf("And this is the output: %s", string(j)))
 
-	//fmt.Fprintln(m.output, string(j))
 	fmt.Println(string(j))
 	return nil
 }
